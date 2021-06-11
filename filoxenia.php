7f448e45ac5f88ca8165ddcdfccfb892ed21c589
Joe Mak <iwanmak1109@gmail.com>
2021-06-11 16:33:18 +0300

    filoxenia

--- filoxenia.php
+++ filoxenia.php
@@ -1,3 +1,16 @@
+<?php 
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'db_mimp');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Could not connect to database");
+$age = $sex = $size = "";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
